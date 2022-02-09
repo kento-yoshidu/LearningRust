e312074d5b79a5c5895fe298af9e10c1a5f51fb9
kento <kento@toriwatari.bird>
2022-02-09 21:08:05 +0900

    ✨ Update

--- Hello_World/target/debug/deps/Hello_World-a6d2316dfb4ae0ce.d
+++
@@ -1,5 +0,0 @@
-C:\github\LearningRust\Hello_World\target\debug\deps\Hello_World-a6d2316dfb4ae0ce.rmeta: src\main.rs
-
-C:\github\LearningRust\Hello_World\target\debug\deps\Hello_World-a6d2316dfb4ae0ce.d: src\main.rs
-
-src\main.rs:
