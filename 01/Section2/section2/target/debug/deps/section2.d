ded15ac9b78500a19f7a171a1d3221679e54dc87
kento <kento@toriwatari.bird>
2022-07-19 20:24:48 +0900

    ✨ Update

--- 01/Section2/section2/target/debug/deps/section2.d
+++
@@ -1,6 +0,0 @@
-C:\github\LearningRust\01\Section2\section2\target\debug\deps\section2.exe: src\main.rs src\vars.rs
-
-C:\github\LearningRust\01\Section2\section2\target\debug\deps\section2.d: src\main.rs src\vars.rs
-
-src\main.rs:
-src\vars.rs:
