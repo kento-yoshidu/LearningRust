4d88eb521cbc9e1a670959e89313b42bc3fc2705
kento <kento@toriwatari.bird>
2022-07-23 20:06:53 +0900

    ✨ Update Article

--- 01/Section2/section2/src/main.rs
+++ 01/Section2/section2/src/main.rs
@@ -1,6 +1,11 @@
-mod vars;
-
 fn main() {
-    // println!("Hello, world!");
-    vars::vars();
+    let mut s1 = String::from("hello");
+    let mut s2 = String::from("helloworld");
+
+    // ヒープメモリーアドレス
+    println!("{:?}", s1.as_ptr());
+    println!("{}", s1.len());
+    println!("{}", s1.capacity());
+    
+    s1.push_str("_new1");
 }
