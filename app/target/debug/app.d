a68bcbf326fb484a31ccae6feff965b4d0978fe4
kento <kento@toriwatari.bird>
2023-01-09 12:05:08 +0900

    ✨ Update

--- app/target/debug/app.d
+++
@@ -1,1 +0,0 @@
-C:\github\LearningRust\app\target\debug\app.exe: C:\github\LearningRust\app\src\main.rs
--- app/target/debug/deps/app.d
+++
@@ -1,5 +0,0 @@
-C:\github\LearningRust\app\target\debug\deps\app.exe: src\main.rs
-
-C:\github\LearningRust\app\target\debug\deps\app.d: src\main.rs
-
-src\main.rs:
