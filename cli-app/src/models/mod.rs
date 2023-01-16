0d26c33d5757633e8784396cdcc4d221dc57acab
kento <kento@toriwatari.bird>
2023-01-16 20:04:50 +0900

    ✨ Update

--- cli-app/src/models/mod.rs
+++ cli-app/src/models/mod.rs
@@ -35,4 +35,22 @@ impl Item {
     pub fn new(name: String, category: Category, price: u32, date: NaiveDate) -> Self {
         Item {name, category, price, date}
     }
+
+    pub fn get_category(register_type: u8, category_type: u8) -> Category {
+        if register_type == 0 {
+            match category_type {
+                0 => Category::Income(IncomeCategory::Salary),
+                1 => Category::Income(IncomeCategory::Bonus),
+                2 => Category::Income(IncomeCategory::Other),
+                _ => panic!("不正なカテゴリー種別です")
+            }
+        } else {
+            match category_type {
+                0 => Category::Expense(ExpensiveCategory::Food),
+                1 => Category::Expense(ExpensiveCategory::Hobby),
+                2 => Category::Expense(ExpensiveCategory::Other),
+                _ => panic!("不正なカテゴリー種別です")
+            }
+        }
+    }
 }
