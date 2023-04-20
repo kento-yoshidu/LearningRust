1a25f538275d0e8c23e7a2298ccce2a5ef72536c
kento <kento@toriwatari.bird>
2023-04-21 07:14:59 +0900

    🥫 Update

--- md/src/main.rs
+++
@@ -1,6 +0,0 @@
-mod my_type;
-
-fn main() {
-    // println!("Hello, world!");
-    my_type::vec::run();
-}
--- playground/src/my_type/vec.rs
+++ playground/src/my_type/vec.rs
@@ -2,8 +2,6 @@
 // Vec<T>は構造体
 
 pub fn run() {
-    // インスタンス生成
-    // Vec<i32>と推論される
     let mut vec = vec![0, 1, 2, 3];
 
     println!("1: vec {:?}", vec);
@@ -16,4 +14,12 @@ pub fn run() {
 
     // &vec[start..end]で部分列を取り出すことができる
     println!("4: スライスで値を取り出す {:?}", &vec[0..2]);
+
+    for num in vec {
+        println!("x: for-in文を使ってvecの値を出力する {}", num);
+    }
+
+    // println!("[Error] {:?}", vec);
 }
+
+// https://qiita.com/kanna/items/ea5b15f1b4ce0fee2ab3
