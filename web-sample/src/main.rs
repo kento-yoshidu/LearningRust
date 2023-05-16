006203709df4b9516b0fdf53c9f0a199fd33f246
kento <kento@toriwatari.bird>
2023-05-16 19:47:08 +0900

    ✨ Update

---
+++ web-sample/src/main.rs
@@ -0,0 +1,19 @@
+use std::format;
+
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/hello")]
+async fn hello_world() -> impl Responder {
+    format!("Hello World")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello_world)
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
+}
