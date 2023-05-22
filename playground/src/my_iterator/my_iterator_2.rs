45e574aee88c52a7c43f591c00b4535dd31b979b
kento <kento@toriwatari.bird>
2023-05-22 20:09:41 +0900

    ✨ Update

--- playground/src/my_iterator/my_iterator_2.rs
+++ playground/src/my_iterator/my_iterator_2.rs
@@ -1,3 +1,5 @@
+use itertools::{Itertools, MinMaxResult};
+
 pub fn run() {
     /* next */
     // 次の要素をSome<T>で返す
@@ -88,20 +90,125 @@ pub fn run() {
     /* step_by */
     // n個ごとに返す
     // 競プロの文字列操作で使えそう
-    let vec4 = String::from("Hello World");
+    let vec6 = String::from("Hello World");
 
     // chars() = 文字列を分割したIteratorが返る
     // Iterator<char>
-    let mut ite4 = vec4.chars().step_by(2);
+    let mut iter6 = vec6.chars().step_by(2);
 
-    println!("ite4 = {:?}", ite4.next());
+    println!("iter6 = {:?}", iter6.next());
     //=> ite4 = Some('H')
-    println!("ite4 = {:?}", ite4.next());
+    println!("iter6 = {:?}", iter6.next());
     //=> ite4 = Some('l')
-    println!("ite4 = {:?}", ite4.next());
+    println!("iter6 = {:?}", iter6.next());
     //=> ite4 = Some('o')
-    println!("ite4 = {:?}", ite4.next());
+    println!("iter6 = {:?}", iter6.next());
     //=> ite4 = Some('W')
+
+    /* sum */
+    // iteratorの合計値を求める
+    let vec7 = vec![1, 2, 3, 4, 5];
+
+    let mut iter7 = vec7.iter();
+
+    // sumの戻り値には型注釈が必要
+    println!("iter7にsumを適用 {}\n", iter7.sum::<i32>());
+    //=> iter7にsumを適用 15
+
+    // 合計値を吐く、という挙動だけ見ればIteratorのメソッドっぽくないが、next()を呼び出して順々に足し算している
+    // 試しにVec<i32>でsum()を呼び出そうとすると、Iteratorではない、というエラーメッセージが出力される
+    // println!("🦀❌ vec7にsumを適用? {}", vec7.sum::<i32>());
+    //                                      ^^^ `Vec<i32>` is not an iterator; try calling `.into_iter()` or `.iter()`
+
+    /* take */
+    // n個分の要素を取り出し、Iterator<T>で返す
+    let vec = vec![1, 2, 3, 4, 5];
+
+    let mut iter = vec.iter().take(3);
+
+    println!("takeで先頭3つを取り出す = {:?}", iter.next());
+    //=> takeで先頭3つを取り出す = Some(1)
+    println!("takeで先頭3つを取り出す = {:?}", iter.next());
+    //=> takeで先頭3つを取り出す = Some(2)
+    println!("takeで先頭3つを取り出す = {:?}\n", iter.next());
+    //=> takeで先頭3つを取り出す = Some(3)
+
+    /* skip */
+    // n個分の要素をスキップし、それ以降をIterator<T>で返す
+    let vec8 = vec![1, 2, 3, 4, 5];
+
+    // 1, 2, 3をスキップする
+    // Iterator<i32>
+    let mut iter8 = vec8.iter().skip(3);
+
+    println!("iter8 = {:?}", iter8.next());
+    //=> iter8 = Some(4)
+    println!("iter8 = {:?}\n", iter8.next());
+    //=> iter8 = Some(5)
+
+    /* count */
+    // Iteratorの要素数を返す
+    // Iterator.next()が最初にNoneを返すまでカウントしている
+
+    let vec9 = vec![1, 2, 3, 4, 5];
+
+    // usize
+    println!("vec9の要素数 = {}\n", vec9.iter().count());
+    //=> vec9の要素数 = 5
+
+    /* max_by */
+    // よく分からない
+    /*
+    let vec10 = [1, 3, 5, 4, 2];
+
+    let mut iter10 = vec10.iter().max_by(|a, b| {
+        println!("a = {}", a);
+        println!("b = {}", b);
+        b.cmp(a)
+    });
+
+    println!("{:?}", iter10);
+    */
+
+    /* position_max */
+    // 最大のものの位置(index)を返す
+    let vec11 = [1, 3, 5, 4, 2];
+
+    // Iterator<i32>
+    println!("vec11の最大値のindex = {:?}\n", vec11.iter().position_max());
+    //=> vec11の最大値のindex = Some(2)
+
+    /* position_min */
+    let vec12 = [1, 3, 5, 4, 2];
+
+    println!("vec12の最小値のindex = {:?}\n", vec12.iter().position_min());
+    //=> vec12の最小値のindex = Some(0)
+
+    /* position_minmax */
+    // 最小値のindexと最大値のindexを取得する
+    // MinMaxResult型のタプルが返る
+    let vec13 = vec![1, 3, 5, 4, 2];
+
+    //  MinMaxResult<usize>
+    let iter13 = vec13.iter().position_minmax();
+
+    // Result型の一種?なので、unwrapで取り出す
+    println!("vec13の最小値のindex = {}, 最大値のindex = {}\n", iter13.into_option().unwrap().0, iter13.into_option().unwrap().1);
+    //=> vec13の最小値のindex = 0, 最大値のindex = 2
+
+
+    /* 実践 */
+    // 偶数を取り出し、それぞれを2倍して、その合計値を求める
+    let vec = vec![1, 2, 3, 4, 5, 6, 7, 8];
+
+    let mut total: i32 = vec.iter()
+                    .filter(|&x| x % 2 == 0)
+                    .map(|&x| x * 2)
+                    .sum();
+
+    println!("{}", total);
+    //=> 40
 }
 
-// https://qiita.com/lo48576/items/34887794c146042aebf1#%E4%B8%AD%E8%BA%AB%E3%82%92%E5%BC%84%E3%82%89%E3%81%9A%E8%A6%81%E7%B4%A0%E3%82%92%E9%81%B8%E6%8A%9E%E3%81%99%E3%82%8B%E7%B3%BB
+// https://qiita.com/lo48576/items/34887794c146042aebf1
+
--- playground/src/my_result/my_result.rs
+++ playground/src/my_result/my_result.rs
@@ -1,5 +1,6 @@
 /* 基本文法 */
 
+#[cfg(dead_code)]
 fn devide(x: i32, y: i32) -> i32 {
     x / y
 }
