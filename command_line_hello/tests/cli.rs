df2cdbf37e5f3d6580f8fdb5fc5703e05ed1469e
kento <kento@toriwatari.bird>
2024-01-21 17:16:46 +0900

    ⚡ Update

---
+++ command_line_hello/tests/cli.rs
@@ -0,0 +1,21 @@
+use assert_cmd::Command;
+
+#[test]
+fn runs() {
+    // command_line_helloというバイナリが見つかるか
+    let mut cmd = Command::cargo_bin("command_line_hello").unwrap();
+    cmd.assert().success().stdout("Hello, World\n");
+}
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
+
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}
