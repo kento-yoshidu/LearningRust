f37b65ee23cd60cdfaac74f2de3740a0c80bd4c5
kento <kento@toriwatari.bird>
2024-01-27 12:58:43 +0900

    ⚡ Update

--- command_line_cat/src/lib.rs
+++ command_line_cat/src/lib.rs
@@ -4,7 +4,6 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
 #[derive(Debug)]
-#[allow(unused)]
 pub struct Config {
     files: Vec<String>,
     number_lines: bool,
@@ -15,13 +14,26 @@ type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn run(config: Config) -> MyResult<()> {
     for file_name in config.files {
-        println!("file_name = {}", file_name);
         match open(&file_name) {
             Err(err) => eprintln!("Failed to open {}: {}", file_name, err),
             Ok(file) => {
+                let mut current_num = 0;
+
                 for (line_num, line_result) in file.lines().enumerate() {
-                    println!("{}", line_num);
-                    println!("{:?}", line_result);
+                    let line = line_result?;
+
+                    if config.number_lines {
+                        println!("{:>6}\t{}", line_num + 1, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            current_num += 1;
+                            println!("{:>6}\t{}", current_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
                 }
             }
         }
@@ -69,6 +81,6 @@ pub fn get_args() -> MyResult<Config> {
     Ok(Config {
         files: matches.values_of_lossy("files").unwrap(),
         number_lines: matches.is_present("number"),
-        number_nonblank_lines: matches.is_present("number-nonblank"),
+        number_nonblank_lines: matches.is_present("number_nonblank"),
     })
 }
