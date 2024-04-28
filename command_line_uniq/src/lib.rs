2fa17d56205df9e72e833a291a06b444f2166251
kento <kento@toriwatari.bird>
2024-04-28 10:38:57 +0900

    ⚡ Update

---
+++ command_line_uniq/src/lib.rs
@@ -0,0 +1,93 @@
+use clap::{App, Arg};
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader}
+};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    in_file: String,
+    out_file: Option<String>,
+    is_show_count: bool,
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    let print = |count: u64, text: &str| {
+        if count > 0 {
+            if config.is_show_count {
+                print!("{:>4} {}", count, text);
+            } else {
+                print!("{}", text);
+            }
+        }
+    };
+
+    let mut file = open(&config.in_file)
+        .map_err(|e| format!("{}: {}", config.in_file, e))?;
+    let mut line = String::new();
+    let mut previous = String::new();
+    let mut count = 0;
+
+    loop {
+        let bytes = file.read_line(&mut line)?;
+
+        if bytes == 0 {
+            break;
+        }
+
+        if line.trim_end() != previous.trim_end() {
+            print(count, &previous);
+            previous = line.clone();
+            count = 0;
+        }
+
+        count += 1;
+        line.clear();
+    }
+
+    print(count, &previous);
+
+    Ok(())
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("command_line_uniq")
+        .version("0.1.0")
+        .author("Kento Yoshizu")
+        .about("Rust uniq")
+        .arg(
+            Arg::with_name("in_file")
+                .value_name("IN_FILE")
+                .help("Input file")
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("out_file")
+                .value_name("OUT_FILE")
+                .help("Output file")
+        )
+        .arg(
+            Arg::with_name("count")
+                .short("c")
+                .help("Show counts")
+                .long("count")
+                .takes_value(false)
+        )
+        .get_matches();
+
+    Ok(Config {
+        in_file: matches.value_of_lossy("in_file").unwrap().to_string(),
+        out_file: matches.value_of("out_file").map(String::from),
+        is_show_count: matches.is_present("count")
+    })
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
