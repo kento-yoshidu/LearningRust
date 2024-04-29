dc091fcc06cccb2e0789bcc365c5583a81bda053
kento <kento@toriwatari.bird>
2024-04-29 18:55:55 +0900

    ⚡ Update

---
+++ command_line_find/src/lib.rs
@@ -0,0 +1,94 @@
+use crate::EntryType::*;
+use clap::{App, Arg};
+use regex::Regex;
+use std::{arch::x86_64::_MM_FROUND_CEIL, error::Error};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug, Eq, PartialEq)]
+enum EntryType {
+    Dir,
+    File,
+    Link,
+}
+
+#[derive(Debug)]
+pub struct Config {
+    paths: Vec<String>,
+    names: Vec<Regex>,
+    entry_types: Vec<EntryType>,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("command_line_find")
+        .version("0.1.0")
+        .author("Kento Yoshizu")
+        .about("Rust find")
+        .arg(
+            Arg::with_name("paths")
+                .value_name("PATH")
+                .help("Search paths")
+                .default_value(".")
+                .multiple(true)
+        )
+        .arg(
+            Arg::with_name("names")
+                .value_name("NAMES")
+                .short("n")
+                .long("name")
+                .help("Name")
+                .takes_value(true)
+                .multiple(true)
+        )
+        .arg(
+            Arg::with_name("types")
+                .value_name("TYPES")
+                .short("t")
+                .long("type")
+                .help("Entry type")
+                .possible_values(&["f", "d", "l"])
+                .takes_value(true)
+                .multiple(true)
+        )
+        .get_matches();
+
+    let names = matches
+        .values_of_lossy("names")
+        .map(|vals| {
+            vals.into_iter()
+                .map(|name| {
+                    Regex::new(&name)
+                        .map_err(|_| format!("Invalid --name \"{}\"", name))
+                })
+                .collect::<Result<Vec<_>, _>>()
+        })
+        .transpose()? //OptionからResultへの変換
+        .unwrap_or_default();
+
+    let entry_types = matches
+        .values_of_lossy("types")
+        .map(|vals| {
+            vals.iter()
+                .map(|val| {
+                    match val.as_str() {
+                        "d" => Dir,
+                        "f" => File,
+                        "l" => Link,
+                        _ => unreachable!("Invalid type"),
+                    }
+                })
+                .collect()
+        })
+        .unwrap_or_default();
+
+    Ok(Config {
+        paths: matches.values_of_lossy("paths").unwrap(),
+        names,
+        entry_types
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:?}", config);
+    Ok(())
+}
