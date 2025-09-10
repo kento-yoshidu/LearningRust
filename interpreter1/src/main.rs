8872cd936e99ea1cfe04d64b29f0b52a03fe1440
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2025-09-10 21:25:30 +0900

    ⚡ Update

--- interpreter1/src/main.rs
+++ interpreter1/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Write};
+
 trait StackTrait<T> {
     fn push(&mut self, v: T);
     fn pop(&mut self) -> Option<T>;
@@ -34,15 +36,17 @@ impl<T: std::fmt::Debug> StackTrait<T> for Stack<T> {
 
         };
 
-        match self.stack.pop() {
-            Some(res2) => Some((res1, res2)),
+        let res2 = match self.stack.pop() {
+            Some(v) => v,
             None => {
                 println!("スタックにデータがありません。");
                 self.stack.push(res1);
-                None
+                return None;
             }
 
-        }
+        };
+
+        Some((res2, res1))
     }
 
     fn print(&self) {
@@ -58,20 +62,71 @@ impl<T> Stack<T> {
 
 fn main() {
     let mut stack = Stack::new();
-    stack.push(1);
-    stack.print();
-    stack.push(2);
-    stack.print();
 
-    stack.pop();
-    stack.print();
+    loop {
+        print!("-> ");
+        std::io::stdout().flush().unwrap();
 
-    stack.push(3);
-    stack.print();
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        let line = line.trim();
 
-    stack.pop2();
-    stack.print();
+        if line == "printstack" {
+            stack.print();
+            continue;
+        }
 
-    stack.pop();
+        if line == "quit" || line == "exit" {
+            break;
+        }
+
+        if line.to_lowercase() == "exit" {
+            println!("もしかしてexit");
+        }
+        if line.to_lowercase() == "quit" {
+            println!("もしかしてquit");
+        }
+
+        match line {
+            "+" => {
+                if let Some((a, b)) = stack.pop2() {
+                    let c = a + b;
+                    stack.push(c);
+                    println!("{c}");
+                }
+            },
+            "-" => {
+                if let Some((a, b)) = stack.pop2() {
+                    let c = a - b;
+                    stack.push(c);
+                    println!("{}", c);
+                }
+            }
+            "*" => {
+                if let Some((a, b)) = stack.pop2() {
+                    let c = a * b;
+                    stack.push(c);
+                    println!("{}", c);
+                }
+            }
+            "/" => {
+                if let Some((a, b)) = stack.pop2() {
+                    let c = a / b;
+                    stack.push(c);
+                    println!("{}", c);
+                }
+            },
+            _ => {
+                match line.parse::<f64>() {
+                    Ok(v) => {
+                        stack.push(v);
+                        println!("{v}");
+                    },
+                    Err(_) => {
+                        println!("シンタックスエラー");
+                    }
+                }
+            }
+        }
+    }
 }
-
